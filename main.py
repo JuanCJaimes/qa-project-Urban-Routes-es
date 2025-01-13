efd426940ff69472370ba11eba610dbe652981ba
Juan Camilo Jaimes <juancamilojaimes@Ordenador-portatil-de-Juan.local>
2025-01-13 02:43:47 -0500

    Aplicando correcciones y resolviendo conflictos

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 def test_order_taxi():
     # Inicializar el navegador
     driver = webdriver.Chrome()
